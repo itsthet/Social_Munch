df44845619295370d0ab284798705f48092174ab
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2024-05-16 13:57:54 +0100

    changes in heart controller

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -7,19 +7,21 @@ class FavouritesController < ApplicationController
   end
 
   def create
-    @favourite = Favourite.new
-    @favourite.recipe = @recipe
-    @favourite.user = current_user
-    @favourite.save
+    @favourite = Favourite.new(recipe: @recipe, user: current_user)
+    if @favourite.save
+      render json: { message: 'Added to favorites successfully' }, status: :created
+    else
+      render json: { error: 'Failed to add to favorites' }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @favourite = Favourite.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
+    @favourite = Favourite.find(params[:id])
     if @favourite
       @favourite.destroy
-      redirect_to @favourite.recipe, notice: 'Favourite removed successfully'
+      # notice: 'Favourite removed successfully'
     else
-      redirect_to @recipe, alert: 'Favourite not found'
+      render json: { error: 'Favourite not found' }, status: :not_found
     end
   end
 
