d3b815eecb6557e42d1f69211951364f8eb2ef2d
Thet Soe <tps.thetsoe@gmail.com>
2024-05-22 01:04:16 +0800

    edits on the homepage

--- app/javascript/controllers/typedjs_controller.js
+++ app/javascript/controllers/typedjs_controller.js
@@ -6,17 +6,11 @@ export default class extends Controller {
   static targets = [
     "input"
   ]
-  connect() {
-    console.log(this.inputTarget)
-    const typed = new Typed(this.inputTarget, {
-      strings: ["Type Your Ingredients ..."],
-      typeSpeed: 50,
-    });
-
-  //   return () => {
-  //     // Destroy Typed instance during cleanup to stop animation
-  //     typed.destroy();
-  //   };
-  // }, [];
-  }
+  // connect() {
+  //   console.log(this.inputTarget)
+  //   const typed = new Typed(this.inputTarget, {
+  //     strings: ["Type Your Ingredients ..."],
+  //     typeSpeed: 50,
+  //   });
+  // }
 }
